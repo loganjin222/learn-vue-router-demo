0b9501f49a2b5fbe5888b0fbc65712bce0e3a4cc
loganjin <loganjin222@gmail.com>
2023-04-14 21:28:02 +0800

    第二种跳转方式: 通过代码的方式修改路由

--- vue-router-demo/src/App.vue
+++ vue-router-demo/src/App.vue
@@ -7,8 +7,11 @@
     <!-- 当<router-link>对应的路由匹配成功后，会自动给当前元素设置一个为router-link-active的class，通过设置active-class的值可以把自动生成的router-link-active换成你设置的这个值 -->
     <!-- 此时无法统一修改，每个<router-link>都要有active-class="active" -->
     <!-- 开发时用的很少 -->
-    <router-link to="/home" tag="button" replace active-class="active">首页</router-link>
-    <router-link to="/about" tag="button" replace active-class="active">关于</router-link>
+    <!-- <router-link to="/home" tag="button" replace active-class="active">首页</router-link> -->
+    <!-- <router-link to="/about" tag="button" replace active-class="active">关于</router-link> -->
+    
+    <button @click="homeClick">首页</button>
+    <button @click="aboutClick">关于</button>
 
     <!-- 该标签会根据当前的路径，动态渲染出不同的路由组件，它决定路由组件在页面中的位置（路由切换时，切换的是<router-view>挂载的路由组件，其他内容不会改变） -->
     <router-view></router-view>
@@ -17,7 +20,21 @@
 
 <script>
 export default {
-  name: 'App'
+  name: 'App',
+  
+  // 通过代码的方式修改路由
+  methods: {
+    homeClick() {
+      // this.$router.push('/home')
+      this.$router.replace('/home')
+      console.log('homeClick');
+    },
+    aboutClick() {
+      // this.$router.push('/about')
+      this.$router.replace('/about')
+      console.log('aboutClick');
+    }
+  }
 }
 </script>
 
