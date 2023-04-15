44eed92dc3c47ad7846fd239fa73181b6975321f
loganjin <loganjin222@gmail.com>
2023-04-15 15:45:57 +0800

    路由懒加载

--- vue-router-demo/src/router/index.js
+++ vue-router-demo/src/router/index.js
@@ -4,14 +4,16 @@ import Vue, { useAttrs } from 'vue'
 // 1. 导入路由对象；并且调用Vue.use()，通过Vue.use(插件)来安装插件
 import Router from 'vue-router'
 
-import Home from '../components/home'
-import About from '../components/about'
+// import Home from '../components/home'
+// import About from '../components/about'
 
-import User from '../components/user'
+// import User from '../components/user'
 
 Vue.use(Router)
 
-
+// 我们知道路由中通常会定义很多不同的页面(路由组件)，这些页面最后被打包在哪里呢？一般情况下是放在一个JS文件中。
+// 但是这么多页面放在一个JS文件中，必然会造成这个JS文件非常的大，如果我们一次性从服务器请求下来这个页面，可能需要花费一定的时间，甚至用户的电脑上还可能出现短暂空白的情况。如何避免这种情况呢？使用路由懒加载就可以了。
+// 路由懒加载的主要作用就是将路由对应的路由组件打包成一个个的JS文件，只有在这个路由被访问到的时候，才加载对应的路由组件。
 const routes = [
     // 此前没有实现默认显示首页组件，还需要用户点击。通过以下设置可以实现默认显示首页组件
     {
@@ -23,15 +25,18 @@ const routes = [
     },
     {
         path: '/home',
-        component: Home
+        // component: Home
+        component: () => import('../components/home')
     },
     {
         path: '/about',
-        component: About
+        // component: About
+        component: () => import('../components/about')
     },
     {
         path: '/user/:id',
-        component: User
+        // component: User
+        component: () => import('../components/user')
     }
 ]
 
